5aab9ebbba54541ac691c66ae94c15a46ce6c4b6
Rahim Shekh <rahimshekh46751@gmail.com>
2025-01-24 21:23:15 +0530

    Add files via upload

---
+++ program49.java
@@ -0,0 +1,26 @@
+/*
+    Write a program which accept string from user and display it in Reverse order.
+    Input :  "MarvellouS"
+
+    Output : "SuollevraM"
+
+*/
+import java.util.*;
+
+class program49
+{
+    public static void Reverse(String Str)
+    {
+        
+    }
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String:");
+        String str = sobj.nextLine();
+
+        Reverse(str);
+   
+    }
+}
