6d743513154d162aecbacb134a835a7c0f723a08
RahimShekh <rahimshekh46751@gmail.com>
2025-12-08 20:23:36 +0530

    push

--- StringMedium/program1.java
+++ StringMedium/program1.java
@@ -15,3 +15,39 @@ Example 3:
 Input: s = "()(())((()()))"
 Output: 3
 */
+
+import java.util.*;
+
+class program1
+{
+    public static void Logic(String Str)
+    {
+        char arr[] = Str.toCharArray();
+        int iCnt = 0;
+        int maxCount = 0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == '(')
+            {
+                iCnt++;
+            }
+            else if(arr[i] == ')')
+            {
+                maxCount = Math.max(maxCount,iCnt);
+                iCnt--;
+            }
+        }
+
+        System.out.println("the max count of parenthesis:"+maxCount);
+    }
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the string:");
+        String str = sobj.nextLine();
+
+        Logic(str);
+    }
+}
